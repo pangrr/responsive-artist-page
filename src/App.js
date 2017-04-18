03103cf716ea287d3113411608c78877800281f0
rpang <rpang@paypal.com>
2017-04-17 17:45:01 -0700

    backup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 
+let sampleList = [
+	'http://iscale.iheart.com/catalog/artist/744880?ops=fit(250,0)',
+	'http://iscale.iheart.com/catalog/artist/57706?ops=fit(250,0)',
+	'http://iscale.iheart.com/catalog/artist/30005067?ops=fit(250,0)',
+	'http://iscale.iheart.com/catalog/artist/44368?ops=fit(250,0)',
+	'http://iscale.iheart.com/catalog/artist/43557?ops=fit(250,0)',
+	'http://iscale.iheart.com/catalog/artist/33221?ops=fit(250,0)'
+];
+
 class App extends Component {
  	render() {
 		return (
     	<div className="App">
      		<Searchbar />
-        <List list={[1, 2, 3, 4, 5, 6]} />
+        <List list={sampleList} />
       </div>
     );
   }
@@ -25,7 +34,7 @@ class List extends Component {
 		return (
 			<div className="list">
 				{this.props.list.map((item, index) => {
-					return <Item key={index} />;
+					return <Item key={index} img={item} />;
 				})}
 			</div>
 		);
@@ -36,7 +45,9 @@ class Item extends Component {
 	render() {
 		return (
 			<div className="item">
-				<div className="item-content"></div>
+				<div className="item-content">
+					<img src={this.props.img} alt="" className="image"></img>
+				</div>
 			</div>
 		);
 	}
